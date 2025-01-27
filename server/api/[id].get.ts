ccb9d3c84458f91cb4b314f5be01acac0490da26
Nick De Jaeger <nick.de.jaeger@tenforce.com>
2025-01-27 01:58:22 +0100

    upd

--- server/api/[id].get.ts
+++ server/api/[id].get.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL || 'https://lfasxebowcjdtueluxgw.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxmYXN4ZWJvd2NqZHR1ZWx1eGd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc3MzkzMzAsImV4cCI6MjA1MzMxNTMzMH0.ZVd64DylhV3K-hDGPKbGX7FCYKeNc6ZC9XT0nbpxiss'
+const supabaseUrl = process.env.SUPABASE_URL || ''
+const supabaseKey = process.env.SUPABASE_KEY || ''
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default defineEventHandler((event) => {

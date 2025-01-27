e92999cab9e0efb4169056b64d413d43282cccb3
Nick De Jaeger <nick.de.jaeger@tenforce.com>
2025-01-27 01:07:28 +0100

    upd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,13 @@ export default defineNuxtConfig({
         }
     },
 
+    runtimeConfig: {
+		public: {
+            supabaseUrl: process.env.SUPABASE_URL,
+            supabaseKey: process.env.SUPABASE_KEY
+		}
+    },
+
     modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss'],
 
     supabase: {

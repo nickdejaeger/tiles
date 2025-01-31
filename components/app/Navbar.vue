587368ae39cf1c63c8c35fb06f1ccccac57536ec
Nick De Jaeger <nick.de.jaeger@tenforce.com>
2025-01-31 03:14:59 +0100

    upd

--- components/app/Navbar.vue
+++ components/app/Navbar.vue
@@ -9,56 +9,83 @@ const abc = ref({
 <template>
     <nav>
         <div>
-            <div><NuxtLink to="/">HOME</NuxtLink></div>
-            <ul>
-                <li><NuxtLink to="/admin">Admin</NuxtLink></li>/
-                <li><NuxtLink to="/about">About</NuxtLink></li>
-                <li v-for="(link, index) in abc" :key="index">/ <NuxtLink :to="link">{{ link.toUpperCase() }}</NuxtLink></li>
-            </ul>
-        </div>
-        <div>
-            <AppLogoutButton />
+            <div>
+                <div><NuxtLink to="/">Logo</NuxtLink></div>
+                
+            </div>
+            <div>
+                <ul>
+                    <li><NuxtLink to="/admin">Admin</NuxtLink></li>
+                    <li><NuxtLink to="/about">About</NuxtLink></li>
+                    <li v-for="(link, index) in abc" :key="index">
+                        <NuxtLink :to="link">{{ link.toUpperCase() }}</NuxtLink>
+                    </li>
+                </ul>
+                <AppLogoutButton />
+            </div>
         </div>
     </nav>
 </template>
 
 <style scoped lang="scss">
 nav {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 1.5rem 3rem;
+    padding: .5rem 1rem;
     background: #FFF;
     z-index: 3;
-    background-color: #f5d4da;
+    background: #F2F5F0;
     margin: 0 0 2rem;
 
-    div {
+    > div {
         display: flex;
-        align-items: flex-start;
+        align-items: center;
         gap: 3rem;
-    }
 
-    ul {
-        margin: 0;
-        padding: 0;
-        list-style: none;
         display: flex;
-        flex-wrap: wrap;
-        gap: .5rem;
+        align-items: center;
+        justify-content: space-between;
 
-        li {
-            display: inline-flex;
+        max-width: 1200px;
+        margin: 0 auto;
+
+        div {
+            display: flex;
+            align-items: center;
+            gap: 1rem;
+        }
+
+        ul {
+            margin: 0;
+            padding: 0;
+            list-style: none;
+            display: flex;
+            flex-wrap: wrap;
             gap: .5rem;
+
+            li {
+                display: inline-flex;
+                gap: .5rem;
+
+                color: #383835;
+                font-size: .7rem;
+                line-height: 1.742;
+
+                a {
+                    text-decoration: underline;
+                    text-decoration-thickness: 1px;
+                    text-underline-offset: 2px;
+                }
+            }
         }
     }
+
+    
 
     button {
         border: 0;
         background-color: #e51919;
         color: white;
         border-radius: 0.25rem;
-        padding: 0.35rem 0.5rem;
+        padding: 0.15rem 0.25rem;
         cursor: pointer;
         font-size: 13px;
 
